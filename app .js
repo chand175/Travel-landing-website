dbd6d581e6bd622fb34a67025cce817806e99a64
chand175 <skchand175@gmail.com>
2024-06-24 01:08:50 +0530

    hambarger

---
+++ app .js
@@ -0,0 +1,55 @@
+// function reversess(sen){
+//     let word= sen.split(" ");
+//     if(word[0]){
+//         word[0]=word[0].split("").reverse().join("");
+//     } return word.join(" ");
+// }
+// console.log(reversess("hello world"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let sen= "javascript is fun";
+// let words= sen.split(" ");
+
+// let newstr=" ";
+
+// for(let item of words){
+//  let sentence= item.charAt(0).toUpperCase()+ item.slice(1);
+//  newstr+=sentence+" ";
+ 
+
+// }
+
+
+// console.log(newstr); 
+
+
+
+
+
+
+
+function showSidebar(){
+  let  sidebar= document.querySelector('.sidebar');
+  sidebar.style.display="flex"
+}
+
+function hideSidebar(){
+    let  sidebar= document.querySelector('.sidebar1');
+    sidebar.style.display="none"
+  }
+
