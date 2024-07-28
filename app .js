813f13813d6c4bc1a141c7e3c5ab7e0f563cf05e
shaik chand basha <skchand175@gmail.comchand175chand175>
2024-07-28 10:05:46 +0530

    commit

--- app .js
+++ app .js
@@ -1,52 +1,12 @@
-// function reversess(sen){
-//     let word= sen.split(" ");
-//     if(word[0]){
-//         word[0]=word[0].split("").reverse().join("");
-//     } return word.join(" ");
-// }
-// console.log(reversess("hello world"))
+
+let hambtn = document.querySelector('.hamburger')
+let boycolor = document.querySelector('.main-body')
+let mobileMenu = document.querySelector('.mobile-menu')
+
+hambtn.addEventListener("click", () => {
+  console.log("this is baji")
+  mobileMenu.style.display = "blobk";
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let sen= "javascript is fun";
-// let words= sen.split(" ");
-
-// let newstr=" ";
-
-// for(let item of words){
-//  let sentence= item.charAt(0).toUpperCase()+ item.slice(1);
-//  newstr+=sentence+" ";
- 
-
-// }
-
-
-// console.log(newstr); 
-
-
-
-
-function showSidebar(){
-  let  sidebar= document.querySelector('.sidebar');
-  sidebar.style.display="flex"
-}
-
-function hideSidebar(){
-    let  sidebar= document.getElementById('closeBtn');
-    sidebar.style.display="none"
-  }
 
